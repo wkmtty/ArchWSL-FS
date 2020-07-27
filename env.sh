1debb632bc768218c5d4eef1da8116e41e8308be
wkmtty <ma.wkmtty@gmail.com>
2020-07-27 12:31:22 +0900

    env.sh:Update fakeroot-tcp v1.24.2

--- env.sh
+++ env.sh
@@ -5,10 +5,10 @@ ROOTFS_VER="2020.07.01"
 ROOTFS_FN="archlinux-bootstrap-${ROOTFS_VER}-${ARCH}.tar.gz"
 ROOTFS_URL="http://mirrors.kernel.org/archlinux/iso/${ROOTFS_VER}/${ROOTFS_FN}"
 
-FRTCP_BLD="18082100"
-FRTCP_VER="1.23-1"
+FRTCP_BLD="20072700"
+FRTCP_VER="1.24-2"
 FRTCP_FN="fakeroot-tcp-${FRTCP_VER}-${ARCH}.pkg.tar.xz"
-FRTCP_URL="https://github.com/yuk7/arch-prebuilt/releases/download/${FRTCP_BLD}/${FRTCP_FN}"
+FRTCP_URL="https://github.com/wkmtty/ArchWSL-FS/releases/download/${FRTCP_BLD}/${FRTCP_FN}"
 
 PAC_PKGS="base less nano sudo vim curl"
 
